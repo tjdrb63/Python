00209c74190df177862a5a5c9f497371585d73db
tjdrb63 <tjdrb63@gmail.com>
2021-09-16 14:01:41 +0900

    9/9 Test

---
+++ Test/for.py
@@ -0,0 +1,62 @@
+# arr= [1,2,3,4]
+
+# for i in arr[0:]:
+#     print(i)
+
+# tp =(1,2,3,4,5)
+# for i in tp:
+#     print(i)
+
+# str = 'Hello EveryOne'
+# for s in str:
+#     print(s)
+
+# count = [1,2,3,4,5,6,7,8,9]
+# cnt1 = 0
+# cnt2 = 0
+
+# for i in count:
+#     if i % 2:
+#         cnt1+=i
+#     else:
+#         cnt2+=i
+
+# print(cnt1)
+# print(cnt2)
+
+# for i in range(1,5):
+#     for j in range(0,i):
+#         print("*",end='')
+#     print()
+
+# count = [1,2,3,4,5]
+# for index , value in enumerate(count):
+#     print(index,value)
+
+# arr1 =[10,20,30,40]
+# arr2 =[1,2,3,4]
+# arr3 =[2,4,6]
+
+# for i,j,k in zip(arr1,arr2,arr3):
+#     print(i,j,k)
+
+# arr= [[i,j] for i in range(3) for j in range(3)]
+# print(arr)
+
+# arr =[0]*1000
+
+# print(arr)
+
+# arr = [i for i in range(1,10,2)]
+# print(arr)
+
+# arr=[i*3 for i in range(1,10)]
+# print(arr)
+
+# n = int(input())
+# arr=[int(input()) for i in range(n)]
+
+# print(arr)
+# arr2=[i for i in arr if i>=0]
+# print(arr2)
+
---
+++ Test/if.py
@@ -0,0 +1,5 @@
+num1,num2 = map(int,input().split())
+count = input()
+
+
+print(num1+num2 if count == '+' else num1-num2)
